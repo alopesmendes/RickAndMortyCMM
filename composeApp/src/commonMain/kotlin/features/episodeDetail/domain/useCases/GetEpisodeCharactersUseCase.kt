c3d7abcfed502a3e77bd050aa96c132f6365cb59
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2024-07-23 11:53:07 +0200

    [#10] :art: get episode characters use case

--- composeApp/src/commonMain/kotlin/features/episodeDetail/domain/useCases/GetEpisodeCharactersUseCase.kt
+++ composeApp/src/commonMain/kotlin/features/episodeDetail/domain/useCases/GetEpisodeCharactersUseCase.kt
@@ -1,8 +1,26 @@
 package features.episodeDetail.domain.useCases
 
+import core.entities.State
+import core.entities.toFailure
+import features.episodeDetail.domain.entities.EpisodeCharacter
 import features.episodeDetail.domain.repositories.EpisodeDetailRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class GetEpisodeCharactersUseCase(
     private val repository: EpisodeDetailRepository
 ) {
+    operator fun invoke(characters: List<String>): Flow<State<List<EpisodeCharacter>>> = flow {
+        try {
+            emit(State.Loading)
+            val result = repository.getCharacters(characters)
+            val state = result.fold(
+                onFailure = { State.Error(it.toFailure()) },
+                onSuccess = { State.Success(it) }
+            )
+            emit(state)
+        } catch (e: Exception) {
+            emit(State.Error(e.toFailure()))
+        }
+    }
 }
