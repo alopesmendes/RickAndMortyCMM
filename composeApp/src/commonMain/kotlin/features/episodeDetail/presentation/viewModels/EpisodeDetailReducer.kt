314d1701acf24c398692fa99a89a0289fd281ced
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2024-07-23 09:06:02 +0200

    [#10] :construction: add episode detail reducer, view model

---
+++ composeApp/src/commonMain/kotlin/features/episodeDetail/presentation/viewModels/EpisodeDetailReducer.kt
@@ -0,0 +1,27 @@
+package features.episodeDetail.presentation.viewModels
+
+import core.util.Reducer
+import features.episodeDetail.domain.useCases.GetEpisodeDetailUseCase
+import features.episodeDetail.mapper.mapTo
+import features.episodeDetail.presentation.intents.EpisodeDetailEffect
+import features.episodeDetail.presentation.intents.EpisodeDetailIntent
+import features.episodeDetail.presentation.state.EpisodeDetailState
+import kotlinx.coroutines.flow.collectLatest
+import kotlin.reflect.KFunction1
+
+class EpisodeDetailReducer(
+    private val getEpisodeDetailUseCase: GetEpisodeDetailUseCase
+): Reducer<EpisodeDetailState, EpisodeDetailIntent, EpisodeDetailEffect> {
+    override suspend fun reduce(
+        updateState: KFunction1<(EpisodeDetailState) -> EpisodeDetailState, Unit>,
+        intent: EpisodeDetailIntent
+    ) {
+        when (intent) {
+            is EpisodeDetailIntent.FetchEpisodeDetail -> {
+                getEpisodeDetailUseCase(intent.id).collectLatest { state ->
+                    updateState.invoke(state::mapTo)
+                }
+            }
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/features/episodeDetail/presentation/viewModels/EpisodeDetailViewModel.kt
@@ -0,0 +1,27 @@
+package features.episodeDetail.presentation.viewModels
+
+import androidx.lifecycle.SavedStateHandle
+import core.util.BaseViewModel
+import core.util.Constants
+import features.episodeDetail.domain.useCases.GetEpisodeDetailUseCase
+import features.episodeDetail.presentation.intents.EpisodeDetailEffect
+import features.episodeDetail.presentation.intents.EpisodeDetailIntent
+import features.episodeDetail.presentation.state.EpisodeDetailState
+
+class EpisodeDetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    getEpisodeDetailUseCase: GetEpisodeDetailUseCase,
+): BaseViewModel<EpisodeDetailState, EpisodeDetailIntent, EpisodeDetailEffect>(
+    initialState = EpisodeDetailState(),
+    reducer = EpisodeDetailReducer(
+        getEpisodeDetailUseCase = getEpisodeDetailUseCase
+    ),
+) {
+    private val episodeId = savedStateHandle.get<Int>(Constants.PARAM_EPISODE_DETAIL)
+
+    init {
+        episodeId?.let {
+            sendIntent(EpisodeDetailIntent.FetchEpisodeDetail(it))
+        }
+    }
+}
